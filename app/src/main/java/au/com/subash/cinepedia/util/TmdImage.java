843c4f2678dafbea990215ffa6e6602580f1db07
Subash Adhikari <me@subash.com.au>
2017-11-05 21:38:16 +1100

    fix image url

--- app/src/main/java/au/com/subash/cinepedia/util/TmdImage.java
+++ app/src/main/java/au/com/subash/cinepedia/util/TmdImage.java
@@ -5,6 +5,6 @@ public class TmdImage {
   private static final String IMAGE_URL = "https://image.tmdb.org/t/p/w";
 
   public static String getImageUrl(String imageUrl, int width) {
-    return IMAGE_URL + Integer.toString(width) + "/" + imageUrl;
+    return IMAGE_URL + Integer.toString(width) + imageUrl;
   }
 }
